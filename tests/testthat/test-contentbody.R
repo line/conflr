da93748d0201698b663c36c27aae4f5834f977e2
Hiroaki Yutani <hiroaki.yutani@linecorp.com>
2020-01-08 14:16:43 +0900

    Add confl_contentbody_convert()

---
+++ tests/testthat/test-contentbody.R
@@ -0,0 +1,25 @@
+context("contentbody")
+
+test_that("confl_post_page() works", {
+  # cannot mock on CI
+  skip_on_ci_or_cran()
+
+  res <- structure(list(status_code = 200), class = "response")
+  m <- mockery::mock(res)
+
+  with_mock(
+    "conflr::confl_verb" = m,
+    "httr::content" = function(res) NULL, {
+      confl_contentbody_convert("{cheese}", "wiki", "storage")
+    }
+  )
+
+  args <- mockery::mock_args(m)[[1]]
+  expect_equal(args[[2]], "/contentbody/convert/storage")
+  expect_equal(args$body,
+     list(
+       value = "{cheese}",
+       representation = "wiki"
+     )
+  )
+})

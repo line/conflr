cfdfda61403dbef8904f49783301a2c5939a105c
Hiroaki Yutani <hiroaki.yutani@linecorp.com>
2020-01-20 14:13:58 +0900

    Improve tests

--- tests/testthat/test-front-matter.R
+++ tests/testthat/test-front-matter.R
@@ -2,19 +2,35 @@ should_not_be_called <- function(...) {
   stop(deparse(match.call()[[1]]), "() should not be called", call. = FALSE)
 }
 
-do_confl_create_post_from_Rmd <- function(mock, Rmd_file = "Rmd/front-matter.Rmd", ...) {
+do_confl_create_post_from_Rmd <- function(mock, text, ...) {
+  tmp <- tempfile(fileext = ".Rmd")
+  on.exit(unlink(tmp))
+  
+  writeLines(text, tmp)
   with_mock(
     "conflr:::confl_upload" = mock,
     "conflr:::confl_get_current_user" = function(...) list(username = "user"),
     "conflr:::try_get_personal_space_key" = should_not_be_called, {
-      confl_create_post_from_Rmd(Rmd_file, interactive = FALSE, ...)
+      confl_create_post_from_Rmd(tmp, interactive = FALSE, ...)
     }
   )
 }
 
 test_that("confluence_settings can be set from front-matter", {
   confl_upload_mock <- mockery::mock(NULL)
-  do_confl_create_post_from_Rmd(confl_upload_mock)
+  do_confl_create_post_from_Rmd(confl_upload_mock,
+  '---
+title: "title1"
+confluence_settings:
+  space_key: "space1"
+  parent_id: 1234
+  update: TRUE
+  use_original_size: TRUE
+---
+
+test
+
+')
 
   cols_to_compare <- c("title", "space_key", "parent_id", "update", "use_original_size")
   expect_equal(
@@ -30,6 +46,18 @@ test_that("confluence_settings can be set from front-matter", {
   
   confl_upload_mock <- mockery::mock(NULL)
   do_confl_create_post_from_Rmd(confl_upload_mock,
+    '---
+title: "title1"
+confluence_settings:
+  space_key: "space1"
+  parent_id: 1234
+  update: TRUE
+  use_original_size: TRUE
+---
+
+test
+
+',
     title = "title2", space_key = "space2", parent_id = 9999,
     update = FALSE, use_original_size = FALSE
   )

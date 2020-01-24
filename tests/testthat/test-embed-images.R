f241d7035f4b68de391a292615fcd76253f5702e
Hiroaki Yutani <hiroaki.yutani@linecorp.com>
2020-01-24 11:42:49 +0900

    Fix test expectations

--- tests/testthat/test-embed-images.R
+++ tests/testthat/test-embed-images.R
@@ -23,11 +23,13 @@ test_that("embed_images() works for non-ASCII dir", {
   dir.create(tmp_dir)
   file.copy("plot1.png", file.path(tmp_dir, "plot1.png"))
 
-  # check preview html
   md_text <- "# \u30c6\u30b9\u30c8\n![](%C3%B6/plot1.png)\n"
   html_text <- commonmark::markdown_html(md_text)
+  expected <- stringi::stri_replace_all_fixed(html_text, "%C3%B6/plot1.png", base64_img)
+
+  # unit test
   result1 <- embed_images(html_text, "%C3%B6/plot1.png", file.path(tmp_dir, "plot1.png"))
-  expect_equal(result1, stringi::stri_replace_all_fixed(html_text, "%C3%B6/plot1.png", base64_img))
+  expect_equal(result1, expected)
 
   # integrated test
   Rmd_with_some_settings <-
@@ -36,12 +38,12 @@ confluence_settings:
   space_key: "space1"'
   confl_upload_mock <- mockery::mock(NULL)
   do_confl_create_post_from_Rmd(confl_upload_mock, Rmd_with_some_settings, body = md_text)
-  result2 <- mockery::mock_args(confl_upload_mock)[[1]]$html_text
-  expect_equal(result2, html_text)
-  expect_match(
-    translate_to_confl_macro(result2),
-    '<ac:image ac:width="600"><ri:attachment ri:filename="plot1.png" /></ac:image>',
-    fixed = TRUE
+  result2 <- mockery::mock_args(confl_upload_mock)[[1]]
+
+  expect_equal(result2$html_text, html_text)
+  expect_equal(
+    embed_images(result2$html_text, result2$imgs, result2$imgs_realpath),
+    expected
   )
 })
 

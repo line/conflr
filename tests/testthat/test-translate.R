302b485d664d10013633474235dcc5e42d9a4a78
Hiroaki Yutani <hiroaki.yutani@linecorp.com>
2020-02-04 14:57:06 +0900

    Add license headers in tests (#89)

--- tests/testthat/test-translate.R
+++ tests/testthat/test-translate.R
@@ -1,3 +1,13 @@
+# Copyright (C) 2019 LINE Corporation
+#
+# conflr is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, version 3.
+#
+# conflr is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See <http://www.gnu.org/licenses/> for more details.
+
 test_that("unescape_html() works", {
   expect_equal(unescape_html("evony &amp; ivory"), "evony & ivory")
   expect_equal(unescape_html("x &lt;&gt; &#39;foo&#39;"), "x <> 'foo'")

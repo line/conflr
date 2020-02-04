9388402e286cb2231ca0a93dfa23a71fee30e83e
Hiroaki Yutani <hiroaki.yutani@linecorp.com>
2020-02-04 14:56:00 +0900

    Add license headers in tests

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,3 +1,13 @@
+# Copyright (C) 2019 LINE Corporation
+#
+# conflr is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, version 3.
+#
+# conflr is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See <http://www.gnu.org/licenses/> for more details.
+
 library(testthat)
 library(conflr)
 
--- tests/testthat/test-embed-images.R
+++ tests/testthat/test-embed-images.R
@@ -1,3 +1,13 @@
+# Copyright (C) 2019 LINE Corporation
+#
+# conflr is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, version 3.
+#
+# conflr is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See <http://www.gnu.org/licenses/> for more details.
+
 base64_img <- knitr::image_uri("plot1.png")
 
 test_that("embed_images() works for current dir", {

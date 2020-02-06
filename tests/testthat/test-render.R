f616e48cb144abc429f687f97838e9a459036d19
Hiroaki Yutani <hiroaki.yutani@linecorp.com>
2020-02-06 11:28:42 +0900

    Add a failing test

---
+++ tests/testthat/test-render.R
@@ -0,0 +1,36 @@
+# Copyright (C) 2019 LINE Corporation
+#
+# conflr is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, version 3.
+#
+# conflr is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See <http://www.gnu.org/licenses/> for more details.
+
+test_that("rmarkdown::render() works", {
+  tmp <- tempfile(fileext = ".Rmd")
+  on.exit(unlink(tmp))
+
+  writeLines(
+    "---
+title: title1
+output:
+  conflr::confluence_document:
+    toc: false
+---
+
+# h1
+## h2
+", tmp
+  )
+
+  with_mock(
+    "conflr::confl_get_current_user" = function(...) list(username = "user"),
+    "conflr:::try_get_existing_page_id" = function(...) NULL,
+    "conflr:::try_get_personal_space_key" = should_not_be_called,
+    {
+      expect_error(rmarkdown::render(tmp), "Please provide `space_key`!")
+    }
+  )
+})

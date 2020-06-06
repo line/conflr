490b30c0cb1c8a31293d7ccdb4a71b84f1e8930e
Hiroaki Yutani <hiroaki.yutani@linecorp.com>
2020-06-06 15:26:33 +0900

    Add a license header

--- R/macros.R
+++ R/macros.R
@@ -1,3 +1,13 @@
+# Copyright (C) 2019-2020 LINE Corporation
+#
+# conflr is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, version 3.
+#
+# conflr is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See <http://www.gnu.org/licenses/> for more details.
+
 #' General Confluence macro builder for internal use
 #' @param type inline or block code style to be used for the HTML content
 #' @param name Confluence macro name

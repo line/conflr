36b17070007ef4ab4f3a689748b86674b0dd0688
Hiroaki Yutani <hiroaki.yutani@linecorp.com>
2020-06-11 20:23:23 +0900

    Add more tests

--- tests/testthat/test-tabset.R
+++ tests/testthat/test-tabset.R
@@ -6,6 +6,164 @@ test_that("wrap_tabsets() works", {
   expect_equal(wrap_tabsets("# h1\n# h2 {.some-class .tabset .other-class}\ntest"), expect)
 })
 
+html_doc <- function(x) {
+  x <- commonmark::markdown_html(x)
+  xml2::read_xml(
+    paste0("<body>", x, "</body>"),
+    options = c("RECOVER", "NOERROR", "NOBLANKS")
+  )
+}
+
+test_that("determine_tabset_level() works", {
+  # return NULL for non-tabset document
+  expect_equal(determine_tabset_level(html_doc("# 1\n## 2")), NULL)
+
+  # code chunk
+  html_doc1 <- html_doc(
+    "
+## t1
+
+<tabset-start/>
+
+### t2
+
+content2
+
+### t3
+
+content3
+
+"
+  )
+  expect_equal(determine_tabset_level(html_doc1), 2)
+
+  # invalid case
+  html_doc2 <- html_doc(
+    "
+## t1
+
+<tabset-start/>
+
+### t2
+
+content2
+
+### t3
+
+content3
+
+### invalid
+
+<tabset-start/>
+
+"
+  )
+  expect_warning(
+    expect_equal(determine_tabset_level(html_doc2), 2)
+  )
+
+  # complex case
+  html_doc3 <- html_doc(
+    "
+## t1
+
+# t2
+
+### t3
+
+<tabset-start/>
+
+#### t4
+
+content4
+"
+  )
+
+  expect_equal(determine_tabset_level(html_doc3), 3)
+})
+
+test_that("mark_tabsets() works", {
+  # Do nothing on the document without tabsets
+  expect_null(mark_tabsets(html_doc("## t1\n##t2")))
+
+  html_doc1 <- html_doc(
+    "
+## t1
+
+<tabset-start/>
+
+### t2
+
+content2
+
+### t3
+
+content3
+
+")
+
+  mark_tabsets(html_doc1)
+
+  expect_equal(
+    as.character(html_doc1),
+    '<?xml version="1.0" encoding="UTF-8"?>
+<body>
+  <h2>t1</h2>
+  <tabset-start/>
+  <tabset-tab-first>t2</tabset-tab-first>
+  <p>content2</p>
+  <tabset-tab>t3</tabset-tab>
+  <p>content3</p>
+  <tabset-end/>
+</body>
+')
+
+  html_doc2 <- html_doc(
+    "
+## t1
+
+<tabset-start/>
+
+### t2
+
+content2
+
+## t3
+
+<tabset-start/>
+
+### t4
+
+content4
+
+### t5
+
+## t1
+
+")
+
+  mark_tabsets(html_doc2)
+
+  expect_equal(
+    as.character(html_doc2),
+    '<?xml version="1.0" encoding="UTF-8"?>
+<body>
+  <h2>t1</h2>
+  <tabset-start/>
+  <tabset-tab-first>t2</tabset-tab-first>
+  <p>content2</p>
+  <tabset-end/>
+  <h2>t3</h2>
+  <tabset-start/>
+  <tabset-tab-first>t4</tabset-tab-first>
+  <p>content4</p>
+  <tabset-tab>t5</tabset-tab>
+  <tabset-end/>
+  <h2>t1</h2>
+</body>
+')
+})
+
 test_that("translate_to_confl_macro() can handle tabset", {
   # code chunk
   html_text1 <- commonmark::markdown_html(
